f4a477ced07a8fca1f9bbb40e63d242cbb72c331
viveksharma697 <viveksharma697@gmail.com>
2022-03-18 23:06:11 +0530

    that's all for the day

---
+++ str14.py
@@ -0,0 +1,6 @@
+
+# Write a Python script that takes input from the user and displays that input back in upper and lower cases.
+
+a = input("I love Python the Most, what about you ? ")
+print("I love ", a.upper())
+print("I love ", a.lower())

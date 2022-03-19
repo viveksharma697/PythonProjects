145ada35a4bf246f11df4a98abff732797612a05
viveksharma697 <viveksharma697@gmail.com>
2022-03-20 00:10:58 +0530

    that's all for today

---
+++ str49.py
@@ -0,0 +1,9 @@
+# Write a Python program to swap comma and dot in a string.
+# Sample string: "32.054,23"
+# Expected Output: "32,054.23"
+
+a = "32.054,23"
+b = a.b
+a = a.translate(b(',.', '.,'))
+print(a)
+

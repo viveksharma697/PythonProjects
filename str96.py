ab8009a6914c6a27275a36c729213ec282a78aaf
viveksharma697 <viveksharma697@gmail.com>
2022-03-22 21:58:50 +0530

    string exercises completed

---
+++ str96.py
@@ -0,0 +1,23 @@
+# Write a Python program to convert a given string to camelcase.
+# Sample Output:
+# javascript
+# fooBar
+# fooBar
+# foo.Bar
+# fooBar
+# foobar
+# fooBar
+
+
+from re import sub
+
+def camel_case(s):
+  s = sub(r"(_|-)+", " ", s).title().replace(" ", "")
+  return ''.join([s[0].lower(), s[1:]])
+print(camel_case('JavaScript'))
+print(camel_case('Foo-Bar'))
+print(camel_case('foo_bar'))
+print(camel_case('--foo.bar'))
+print(camel_case('Foo-BAR'))
+print(camel_case('fooBAR'))
+print(camel_case('foo bar'))
